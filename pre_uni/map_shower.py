bcef1eac96cc0d00be89a14eba09f97036697f9a
gotemployabilityrecommendation <gotemployabilityrecommendation@gmail.com>
2025-05-06 17:32:44 +0800

    update

--- pre_uni/map_shower.py
+++ pre_uni/map_shower.py
@@ -227,7 +227,7 @@ class PoiBasedMap:
                     zoom=zoom
                 ),
             )
-            fig.update_layout(width = 350, height = 430)
+            fig.update_layout(width = 350, height = 450)
             # fig.show()
             st.plotly_chart(fig, use_container_width = True)
         else:
